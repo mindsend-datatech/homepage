e8d0999c5d764f031a56dac48e476e282800eb97
chicodias <chicodias@outlook.com>
2025-10-01 10:15:27 -0300

    fix: typo in hyperspeed bg

--- src/components/HyperspeedBackground.tsx
+++ src/components/HyperspeedBackground.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import Hyperspeed from "./Hyperspeed";
 import { Background } from "@/once-ui/components";
 import { effects } from "@/app/resources";
+import { opacity, SpacingToken } from "@/once-ui/types";
 
 export default function HyperspeedBackground() {
   const [opacity, setOpacity] = useState(1);
@@ -101,10 +102,38 @@ export default function HyperspeedBackground() {
         fill
         pointerEvents="none"
         mask={{ ...effects.mask, radius: 120 }}
-        gradient={effects.gradient}
-        dots={effects.dots}
-        grid={effects.grid}
-        lines={effects.lines}
+        gradient={{
+          display: effects.gradient.display,
+          opacity: effects.gradient.opacity as opacity,
+          x: effects.gradient.x,
+          y: effects.gradient.y,
+          width: effects.gradient.width,
+          height: effects.gradient.height,
+          tilt: effects.gradient.tilt,
+          colorStart: effects.gradient.colorStart,
+          colorEnd: effects.gradient.colorEnd,
+        }}
+        dots={{
+          display: effects.dots.display,
+          opacity: effects.dots.opacity as opacity,
+          size: effects.dots.size as SpacingToken,
+          color: effects.dots.color,
+        }}
+        grid={{
+          display: effects.grid.display,
+          opacity: effects.grid.opacity as opacity,
+          color: effects.grid.color,
+          width: effects.grid.width,
+          height: effects.grid.height,
+        }}
+        lines={{
+          display: effects.lines.display,
+          opacity: effects.lines.opacity as opacity,
+          size: effects.lines.size as SpacingToken,
+          thickness: effects.lines.thickness,
+          angle: effects.lines.angle,
+          color: effects.lines.color,
+        }}
       />
     </div>
   );
